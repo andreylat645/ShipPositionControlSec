03d12b67c873af431662b27d2ebaa1b59acea2df
andreylat645 <andrai1998@mail.ru>
2021-10-19 11:33:35 +0300

    Change ModelViewSet, add serializer

--- django_project/control/models.py
+++ django_project/control/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Cruiser(models.Model):
     created = models.DateTimeField(default=timezone.now, null=True)
     title = models.CharField(max_length=60, null=True, blank=False)
-    owner = models.ForeignKey('User', on_delete=models.PROTECT, null=True)
+    owner = models.ForeignKey('User', on_delete=models.PROTECT, null=True, blank=True)
 
     class Meta:
         verbose_name = "Плавательные суда"
@@ -19,7 +19,6 @@ class User(AbstractUser):
     roleId = models.CharField(max_length=100)
     login = models.TextField(null=True, blank=True)
     displayName = models.TextField(null=True, blank=True)
-
 
     def __str__(self):
         return str(self.displayName)
